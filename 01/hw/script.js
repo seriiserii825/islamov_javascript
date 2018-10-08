d464d7ae08d41cd3e1fa5e48d25eb71f97881553
serii <seriiburduja@gmail.com>
2018-10-08 22:51:09 +0300

    finish hw 01

---
+++ 01/hw/script.js
@@ -0,0 +1,36 @@
+//Task1
+
+// let budget = prompt('How much is your budget?', '1000');
+// alert('your budget is: ' + budget);
+
+// let shop = prompt('What is your shop called?', 'Barber');
+// alert('my shop is called ' + shop);
+
+// let mainList = {
+// 	budget: 32000,
+// 	shopName: '',
+// 	shopGoods: [],
+// 	employers: {},
+// 	open: false
+// };
+
+// let firstGood = prompt('What kind of good you will sale?');
+// let secondGood = prompt('What kind of good you will sale?');
+// let thirdGood = prompt('What kind of good you will sale?');
+
+// mainList.shopGoods[0] = firstGood;
+// mainList.shopGoods[1] = secondGood;
+// mainList.shopGoods[2] = thirdGood;
+
+// alert(mainList.shopGoods);
+
+// let dailyBudget = mainList.budget / 30;
+// alert(dailyBudget);
+
+//Task2
+// let num = '33721';
+// let sum = +(num[0])+(+num[1])+(+num[2])+(+num[3])+(+num[4]);
+// let produce = +(num[0])*(+num[1])*(+num[2])*(+num[3])*(+num[4]);
+
+// alert('sum = ' + sum);
+// alert('produce = ' + produce);
